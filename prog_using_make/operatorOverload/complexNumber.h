bb77fd8ab386395535ad475b2bfa154cd0b809bc
mukul29 <joshimukul29@gmail.com>
2018-03-08 02:23:09 +0530

    Inserted comments into main.cpp makefile and complexNumber.h

--- prog_using_make/operatorOverload/complexNumber.h
+++ prog_using_make/operatorOverload/complexNumber.h
@@ -1,16 +1,18 @@
-#ifndef complexNumber_H
+//Header file for complexNumber
+
+#ifndef complexNumber_H //include guard, read on wikipedia
 #define complexNumber_H
 
 class complexNumber
 {
 	int re, im;
 	public:
-	complexNumber(void);
-	complexNumber(int, int);
-void	input(void);
-void	input(int, int);
-void	output(void);
+	complexNumber(void); //default constructor
+	complexNumber(int, int); //explicit constructor
+void	input(void); //input function without arguments
+void	input(int, int); //input function with arguments
+void	output(void); //display elements of an object
 complexNumber operator + (complexNumber);
 	~complexNumber();
 };
-#endif
+#endif //end of include guard
